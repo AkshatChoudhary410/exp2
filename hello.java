51b1354db34ac62df7b3f810e25432983d221cdb
AkshatChoudhary410 <161162523+AkshatChoudhary410@users.noreply.github.com>
2024-08-02 12:42:06 +0530

    Add Java hello_para sample program

--- hello.java
+++ hello.java
@@ -1,9 +1,38 @@
-public class ParameterizedHelloWorld {
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
-            System.out.println("Akshat");
-        }
-    }
+// Java program to illustrate
+// Call by Value
+
+// Callee
+class CallByValue {
+
+	// Function to change the value
+	// of the parameters
+	public static void example(int x, int y)
+	{
+		x++;
+		y++;
+	}
+}
+
+// Caller
+public class Main {
+	public static void main(String[] args)
+	{
+
+		int a = 10;
+		int b = 20;
+
+		// Instance of class is created
+		CallByValue object = new CallByValue();
+
+		System.out.println("Value of a: " + a
+						+ " & b: " + b);
+
+		// Passing variables in the class function
+		object.example(a, b);
+
+		// Displaying values after
+		// calling the function
+		System.out.println("Value of a: "
+						+ a + " & b: " + b);
+	}
 }
